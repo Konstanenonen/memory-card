697af44b55c80cadf5ba5a228c06476093c7f1ad
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-12 00:20:46 +0300

    Added handleClick method so card Components can change the card status of the parent to true when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,30 @@ import Card from "./components/Card";
 
 function App() {
   const [cards, setCards] = useState([
-    { id: 0, name: "0" },
-    { id: 1, name: "1" },
-    { id: 2, name: "2" },
-    { id: 3, name: "3" },
-    { id: 4, name: "4" },
+    { id: 0, name: "first", clicked: false },
+    { id: 1, name: "second", clicked: false },
+    { id: 2, name: "third", clicked: false },
+    { id: 3, name: "fourth", clicked: false },
+    { id: 4, name: "fifth", clicked: false },
   ]);
+
+  const handleCardClick = (id: number) => {
+    const newCards = cards.map((card) =>
+      card.id === id ? { ...card, clicked: true } : card
+    );
+    setCards(newCards);
+  };
 
   return (
     <div className="App">
       <h1>Memory Card Game</h1>
       {cards.map(({ name, id }) => (
-        <Card key={id} id={id} name={name} />
+        <Card
+          key={id}
+          id={id}
+          name={name}
+          handleClick={() => handleCardClick(id)}
+        />
       ))}
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import styled from "styled-components";
 
 interface CardProps {
   id: number;
   name: string;
+  handleClick: () => void;
 }
 
 const Container = styled.div`
@@ -11,14 +13,17 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid black;
 `;
 
-function Card({ id, name }: CardProps) {
+function Card({ id, name, handleClick }: CardProps) {
   return (
-    <Container>
-      <p>{id}</p>
-      <p>{name}</p>
-    </Container>
+    <div>
+      <Container onClick={handleClick}>
+        <p>{id}</p>
+        <p>{name}</p>
+      </Container>
+    </div>
   );
 }
 
