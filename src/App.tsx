1fa2b360d62a4eea271e3f504a186b9bb67554c0
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-12 01:22:46 +0300

    Basic functionality works, needs polish

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import styled from "styled-components";
 import Card from "./components/Card";
 import Scoreboard from "./components/Scoreboard";
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 125px 125px 125px;
+  grid-template-rows: 200px 200px;
+  gap: 10px;
+`;
+
 function App() {
   const [cards, setCards] = useState([
-    { id: 0, name: "first", clicked: false },
-    { id: 1, name: "second", clicked: false },
-    { id: 2, name: "third", clicked: false },
-    { id: 3, name: "fourth", clicked: false },
-    { id: 4, name: "fifth", clicked: false },
+    { id: 0, clicked: false },
+    { id: 1, clicked: false },
+    { id: 2, clicked: false },
+    { id: 3, clicked: false },
+    { id: 4, clicked: false },
+    { id: 5, clicked: false },
   ]);
 
   const total = cards.length;
@@ -21,11 +37,12 @@ function App() {
     // If card is clicked GAME OVER and time to reset
     if (clickedCard?.clicked) {
       setCards([
-        { id: 0, name: "first", clicked: false },
-        { id: 1, name: "second", clicked: false },
-        { id: 2, name: "third", clicked: false },
-        { id: 3, name: "fourth", clicked: false },
-        { id: 4, name: "fifth", clicked: false },
+        { id: 0, clicked: false },
+        { id: 1, clicked: false },
+        { id: 2, clicked: false },
+        { id: 3, clicked: false },
+        { id: 4, clicked: false },
+        { id: 5, clicked: false },
       ]);
       return;
     }
@@ -42,18 +59,15 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <AppContainer>
       <h1>Memory Card Game</h1>
       <Scoreboard score={currentScore} total={total} />
-      {cards.map(({ name, id }) => (
-        <Card
-          key={id}
-          id={id}
-          name={name}
-          handleClick={() => handleCardClick(id)}
-        />
-      ))}
-    </div>
+      <Grid>
+        {cards.map(({ id }) => (
+          <Card key={id} id={id} handleClick={() => handleCardClick(id)} />
+        ))}
+      </Grid>
+    </AppContainer>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import styled from "styled-components";
+import dog1 from "../images/pexels-anna-shvets-4588047.jpg";
+import dog2 from "../images/pexels-gillian-harrison-4668425.jpg";
+import dog3 from "../images/pexels-kasuma-933498.jpg";
+import dog4 from "../images/pexels-lucas-andrade-4681107.jpg";
+import dog5 from "../images/pexels-miguel-constantin-montes-2623968.jpg";
+import dog6 from "../images/pexels-valeria-boltneva-1805164.jpg";
 
 interface CardProps {
   id: number;
-  name: string;
   handleClick: () => void;
 }
 
@@ -16,14 +21,30 @@ const Container = styled.div`
   border: 1px solid black;
 `;
 
-function Card({ id, name, handleClick }: CardProps) {
+function Card({ id, handleClick }: CardProps) {
+  const pictureSrc = () => {
+    switch (id) {
+      case 0:
+        return dog1;
+      case 1:
+        return dog2;
+      case 2:
+        return dog3;
+      case 3:
+        return dog4;
+      case 4:
+        return dog5;
+      case 5:
+        return dog6;
+      default:
+        return dog1;
+    }
+  };
+
   return (
-    <div>
-      <Container onClick={handleClick}>
-        <p>{id}</p>
-        <p>{name}</p>
-      </Container>
-    </div>
+    <Container onClick={handleClick}>
+      <img width={125} height={200} src={pictureSrc()} alt="Dog" />
+    </Container>
   );
 }
 
