e85f5c82aabdf390586c16b1ee0c14e989a5e506
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-13 21:20:12 +0300

    Card order is shuffeled at the game start now

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import NavBar from "./NavBar";
 import Scoreboard from "./Scoreboard";
+import cardArray from "../cardArray";
 
 function App() {
-  const [cards, setCards] = useState([
-    { id: 0, clicked: false },
-    { id: 1, clicked: false },
-    { id: 2, clicked: false },
-    { id: 3, clicked: false },
-    { id: 4, clicked: false },
-    { id: 5, clicked: false },
-    { id: 6, clicked: false },
-    { id: 7, clicked: false },
-    { id: 8, clicked: false },
-    { id: 9, clicked: false },
-    { id: 10, clicked: false },
-    { id: 11, clicked: false },
-  ]);
-
+  const [cards, setCards] = useState(cardArray);
   const total = cards.length;
   const currentScore = cards.filter((card) => card.clicked).length;
 
-  const handleCardClick = (id: number) => {
+  const shuffleArray = <Type extends unknown>(array: Type[]): Type[] => {
+    const arrayCopy = array.slice();
+    return arrayCopy.sort(() => Math.random() - 0.5);
+  };
+
+  const handleCardClick = (id: number): void => {
     const clickedCard = cards.find((card) => card.id === id);
 
     // If card is clicked GAME OVER and time to reset
     if (clickedCard?.clicked) {
-      setCards([
-        { id: 0, clicked: false },
-        { id: 1, clicked: false },
-        { id: 2, clicked: false },
-        { id: 3, clicked: false },
-        { id: 4, clicked: false },
-        { id: 5, clicked: false },
-        { id: 6, clicked: false },
-        { id: 7, clicked: false },
-        { id: 8, clicked: false },
-        { id: 9, clicked: false },
-        { id: 10, clicked: false },
-        { id: 11, clicked: false },
-      ]);
+      setCards(cardArray);
       return;
     }
 
@@ -50,10 +29,16 @@ function App() {
     );
 
     // This shuffles the array
-    newCards.sort(() => Math.random() - 0.5);
+    const shuffledCards = shuffleArray(newCards);
 
-    setCards(newCards);
+    setCards(shuffledCards);
   };
+
+  // On mount shuffle card order
+  useEffect(() => {
+    const shuffeledCards = shuffleArray(cards);
+    setCards(shuffeledCards);
+  }, []);
 
   return (
     <div className="flex flex-col place-items-center">
